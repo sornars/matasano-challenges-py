4ea2feb3146c7a403a1ce2b84fc731a581ac503d
sornars <sornars@users.noreply.github.com>
2014-12-21 01:16:25 +0000

    Modified 01-01 into a function.

--- 01/01.py
+++ 01/01.py
@@ -3,4 +3,7 @@ import binascii
 hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 b64_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
-assert binascii.b2a_base64(binascii.a2b_hex(hex_string)).strip() == b64_string
+def hex2b64(hex_string):
+    return binascii.b2a_base64(binascii.a2b_hex(hex_string)).strip()
+
+assert hex2b64(hex_string) == b64_string
