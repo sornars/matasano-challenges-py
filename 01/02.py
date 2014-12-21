f79b8cbadf560f5af165f6fcb92ca8397a1fcdcb
sornars <sornars@users.noreply.github.com>
2014-12-21 13:13:35 +0000

    Solved challenge 01-02.

---
+++ 01/02.py
@@ -0,0 +1,21 @@
+"""Set 01 - Challenge 02."""
+import binascii
+
+hex_string_1 = '1c0111001f010100061a024b53535009181c'
+hex_string_2 = '686974207468652062756c6c277320657965'
+xor_string = b'746865206b696420646f6e277420706c6179'
+
+def hex_xor(hex_string_1, hex_string_2):
+    """Return the XOR of two hex strings."""
+    if len(hex_string_1) != len(hex_string_2):
+        raise  ValueError('Length of a must be the same as b.')
+
+    hex_data_1 = bytearray.fromhex(hex_string_1)
+    hex_data_2 = bytearray.fromhex(hex_string_2)
+    return binascii.b2a_hex(
+        bytearray(
+            [char_a ^ char_b for char_a, char_b in zip(hex_data_1, hex_data_2)]
+        )
+    )
+
+assert hex_xor(hex_string_1, hex_string_2) == xor_string
