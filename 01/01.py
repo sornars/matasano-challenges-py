5ad7f2677d44145cd21d7a0e0822ab8c55809bbd
sornars <sornars@users.noreply.github.com>
2014-12-21 12:47:17 +0000

    Updated function to work on bytes rather than binascii functions.

--- 01/01.py
+++ 01/01.py
@@ -1,13 +1,14 @@
 """Set 01 - Challenge 01."""
-
-import binascii
+import base64
 
 hex_string = ('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f'
               '69736f6e6f7573206d757368726f6f6d')
-b64_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+b64_string = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 def hex2b64(hex_string):
-    """Convert a hex string into a base64 encoded string."""
-    return binascii.b2a_base64(binascii.a2b_hex(hex_string)).strip()
+    """Convert a hex string into a base64 encoded byte string."""
+    hex_data = bytearray.fromhex(hex_string)
+    # Strip trailing newline
+    return base64.encodebytes(hex_data)[:-1]
 
 assert hex2b64(hex_string) == b64_string
