c717b0efdd165281ad0b21ec79e638c0b480bc95
sornars <sornars@users.noreply.github.com>
2014-12-21 01:14:44 +0000

    Updated README.md. Solved challenge 01-01.

---
+++ 01/01.py
@@ -0,0 +1,6 @@
+import binascii
+
+hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+b64_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+assert binascii.b2a_base64(binascii.a2b_hex(hex_string)).strip() == b64_string
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 matasano-challenges
 ===================
+
+My attempt at solving the matasano-challenges in python.
