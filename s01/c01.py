93b7b679b382cbe6a283e1c711e348891d9d3638
sornars <sornars@users.noreply.github.com>
2014-12-22 15:49:30 +0000

    Changed output to bytestring.

--- s01/c01.py
+++ s01/c01.py
@@ -4,7 +4,7 @@ import binascii
 
 hex_string = ('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f'
               '69736f6e6f7573206d757368726f6f6d')
-b64_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+b64_string = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 def hex2b64(hex_string):
     """Convert a hex string into a base64 encoded string."""
