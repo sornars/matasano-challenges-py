be6f03c6146de8fe05a11b5bf122a3354bbdae83
sornars <sornars@users.noreply.github.com>
2014-12-31 15:59:06 +0000

    Modified 01-05 to work on bytestrings.

--- s01/c05.py
+++ s01/c05.py
@@ -21,14 +21,14 @@ Encrypt a bunch of stuff using your repeating-key XOR function. Encrypt your mai
 import c02
 import binascii
 
-input_string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-key = 'ICE'
+input_string = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+key = b'ICE'
 encrypted_string = b'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
 
 def encrypt_repeating_key_xor(input_string, key):
     """XOR an input_string with a repeating key."""
-    hex_input_string = bytes(''.join(['{:02x}'.format(ord(char)) for char in input_string]).encode())
-    hex_key = bytes(''.join(['{:02x}'.format(ord(char)) for char in key]).encode())
+    hex_input_string = bytes(''.join(['{:02x}'.format(char) for char in input_string]).encode())
+    hex_key = bytes(''.join(['{:02x}'.format(char) for char in key]).encode())
     hex_key = (hex_key * (len(hex_input_string)//len(hex_key)) + hex_key[:(len(hex_input_string) % len(hex_key))])
     return c02.hex_xor(hex_input_string, hex_key)
 
