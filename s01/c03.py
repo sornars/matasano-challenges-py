6a2bb17e27b7660b2b59c1c18b50ff5e05f61356
sornars <sornars@users.noreply.github.com>
2015-01-02 23:18:22 +0000

    Modified decrypt_hex_single_byte_xor to use character frequencies for scoring.

--- s01/c03.py
+++ s01/c03.py
@@ -16,19 +16,56 @@ Achievement Unlocked
 You now have our permission to make "ETAOIN SHRDLU" jokes on Twitter.
 """
 import binascii
-import collections
 import c02
 
 hex_string = (b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a39'
               b'3b3736')
 
-with open('/usr/share/dict/words') as f:
-    ENGLISH_WORDS = tuple(word.upper().strip() for word in f)
+# Character Frequencies taken from
+# http://en.wikipedia.org/wiki/Letter_frequency on 2015-01-02
+# Frequencies for ' ' and numerals extrapolated from article
+CHAR_FREQUENCY = {' ': 12.703,
+                  'E': 12.702,
+                  'T': 9.056,
+                  '0': 8.6115,
+                  '1': 8.6115,
+                  '2': 8.6115,
+                  '3': 8.6115,
+                  '4': 8.6115,
+                  '5': 8.6115,
+                  '6': 8.6115,
+                  '7': 8.6115,
+                  '8': 8.6115,
+                  '9': 8.6115,
+                  'A': 8.167,
+                  'O': 7.507,
+                  'I': 6.966,
+                  'N': 6.749,
+                  'S': 6.327,
+                  'H': 6.094,
+                  'R': 5.987,
+                  'D': 4.253,
+                  'L': 4.025,
+                  'C': 2.782,
+                  'U': 2.758,
+                  'M': 2.406,
+                  'W': 2.360,
+                  'F': 2.228,
+                  'G': 2.015,
+                  'Y': 1.974,
+                  'P': 1.929,
+                  'B': 1.492,
+                  'V': 0.978,
+                  'K': 0.772,
+                  'J': 0.153,
+                  'X': 0.150,
+                  'Q': 0.095,
+                  'Z': 0.074
+                 }
 
 def decrypt_hex_single_byte_xor(hex_string):
     """Decrypt a hex bytestring that has been XOR'd by a single char and return the key, decoded bytestring and word count score."""
-    word_count = collections.defaultdict(int)
-    best_word_count = 0
+    best_score = 0
     best_string = b''
     best_key = 0
     for char in range(0, 256):
@@ -38,20 +75,21 @@ def decrypt_hex_single_byte_xor(hex_string):
 
         xor_data = binascii.a2b_hex(xor_data)
 
+        score = 0
         try:
-            for word in xor_data.decode().upper().split():
-                if word in ENGLISH_WORDS:
-                    word_count[char] += 1
+            for c in xor_data.decode().upper():
+                if c in CHAR_FREQUENCY:
+                    score += CHAR_FREQUENCY[c]
         except UnicodeDecodeError:
             # Skip invalid Unicode
             continue
 
-        if word_count[char] > best_word_count:
-            best_word_count = word_count[char]
+        if score > best_score:
+            best_score = score
             best_key = char
             best_string = xor_data
 
-    return best_key, best_string, best_word_count
+    return best_key, best_string, round(best_score, 2)
 
 if __name__ == '__main__':
-    assert decrypt_hex_single_byte_xor(hex_string) == (88, b"Cooking MC's like a pound of bacon", 6)
+    assert decrypt_hex_single_byte_xor(hex_string) == (88, b"Cooking MC's like a pound of bacon", 214.29)
--- s01/c04.py
+++ s01/c04.py
@@ -17,8 +17,8 @@ with open(os.path.join(os.path.dirname(__file__), 'c04.txt'), 'rb') as f:
         scored_input = c03.decrypt_hex_single_byte_xor(input_hex.strip())
         if scored_input[2] > 0:
             if scored_input[2] > best_fit[3]:
-                best_fit = (input_hex,) + scored_input
+                best_fit = (input_hex.strip(),) + scored_input
 
-assert best_fit == (b'7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f\n',
-                    53, b'Now that the party is jumping\n', 6)
+assert best_fit == (b'7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f',
+                    53, b'Now that the party is jumping\n', 203.74)
 
