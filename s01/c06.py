d00a65f4acc0174bf06021cebb261a50e978a479
sornars <sornars@users.noreply.github.com>
2015-01-03 19:51:34 +0000

    Solved challenge 01-06.

--- s01/c06.py
+++ s01/c06.py
@@ -33,10 +33,16 @@ No, that's not a mistake.
 
 We get more tech support questions for this challenge than any of the other ones. We promise, there aren't any blatant errors in this text. In particular: the "wokka wokka!!!" edit distance really is 37.
 """
+import binascii
+import io
+import os
+import c03
 
 hamming_s1 = b'this is a test'
 hamming_s2 = b'wokka wokka!!!'
 expected_distance = 37
+
+
 
 def hamming_distance(string_1, string_2):
     """Calculate the Hamming Distance between two bytestrings."""
@@ -45,6 +51,51 @@ def hamming_distance(string_1, string_2):
 
     return sum(bin(char_1 ^ char_2).count('1') for char_1, char_2 in zip(string_1, string_2))
 
+def decrypt_repeating_key_xor(input_text):
+    """Decrypt a bytestring that has been encrypted with a repeating key."""
+    keysize_distances = []
+    for keysize in range(2, 41):
+        with io.BytesIO(input_text) as f:
+            block_1 = f.read(keysize)
+            block_2 = f.read(keysize)
+            block_3 = f.read(keysize)
+            block_4 = f.read(keysize)
+            h_distance_1 = hamming_distance(block_1, block_2)
+            h_distance_2 = hamming_distance(block_2, block_3)
+            h_distance_3 = hamming_distance(block_3, block_4)
+            h_distance = (h_distance_1 + h_distance_2 + h_distance_3)/3
+            keysize_distances.append((keysize, h_distance/keysize))
+
+    keysize_distances.sort(key=lambda x: x[1])
+
+    keys = []
+    for keysize, keysize_distance in keysize_distances[:3]:
+        blocks = []
+        with io.BytesIO(input_text) as f:
+            block = f.read(keysize)
+            while block:
+                blocks.append(block)
+                block = f.read(keysize)
+
+        blocks = filter(lambda x: len(x) == keysize, blocks)
+
+        key = b''
+        for transposed_block in zip(*blocks):
+            result = c03.decrypt_hex_single_byte_xor(
+                ''.join(['{:02x}'.format(c) for c in transposed_block]))
+            key += bytes([result[0]])
+
+        keys.append(key)
+
+    return keys
+
+
 
 if __name__ == '__main__':
     assert hamming_distance(hamming_s1, hamming_s2) == expected_distance
+    with open(os.path.join(os.path.dirname(__file__), 'c06.txt'), 'rb') as f:
+        input_text = binascii.a2b_base64(f.read())
+
+    assert b'Terminator X: Bring the noise' in decrypt_repeating_key_xor(input_text)
+
+
