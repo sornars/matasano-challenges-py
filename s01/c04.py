4706caac0709b1c24cde5f2de790bba73f9e803d
sornars <sornars@users.noreply.github.com>
2015-01-04 00:11:10 +0000

    Removed reduntant if statement.

--- s01/c04.py
+++ s01/c04.py
@@ -15,9 +15,8 @@ best_fit = (b'', 0, b'', 0)
 with open(os.path.join(os.path.dirname(__file__), 'c04.txt'), 'rb') as f:
     for input_hex in f:
         scored_input = c03.decrypt_hex_single_byte_xor(input_hex.strip())
-        if scored_input[2] > 0:
-            if scored_input[2] > best_fit[3]:
-                best_fit = (input_hex.strip(),) + scored_input
+        if scored_input[2] > best_fit[3]:
+            best_fit = (input_hex.strip(),) + scored_input
 
 assert best_fit == (b'7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f',
                     53, b'Now that the party is jumping\n', 203.74)
