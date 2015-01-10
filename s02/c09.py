edb6c5fc804189b8c08e2aeb532338e2fdace68c
Shivan Sornarajah <github@sornars.com>
2015-01-10 15:15:06 +0000

    Added optional keyword argument to specify character to pad with in pad_block.

--- s02/c09.py
+++ s02/c09.py
@@ -19,12 +19,13 @@ input_block = b'YELLOW SUBMARINE'
 pad_size = 20
 padded_block = b'YELLOW SUBMARINE\x04\x04\x04\x04'
 
-def pad_block(block, size):
+def pad_block(block, size, char='\x04'):
     """Pad a bytestring to a specified number of bytes."""
     if len(block) > size:
         raise ValueError('Block cannot be larger than size to be padded.')
     elif len(block) < size:
-        block += ('\x04' * (size - len(block))).encode()
+        pad_length = size - len(block)
+        block += (char * pad_length).encode()[:pad_length]
     return block
 
 if __name__ == '__main__':
